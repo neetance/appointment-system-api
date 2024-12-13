188b4028ebfa5fa3af3ee4b564b7b6308de9d178
neetance <ankitbasu14@gmail.com>
2024-12-13 21:52:25 +0530

    added the environment variables and successfully connected the server with the db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 dotenv.config();
@@ -7,6 +8,11 @@ dotenv.config();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error(err));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
